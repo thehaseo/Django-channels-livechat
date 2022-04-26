5763da916e50bd6f647fe80e62ab03434dacd62a
unknown <gregoriazuaje04@gmail.com>
2022-04-26 16:04:11 -0400

    created subclass from AsyncWebsocketConsumer to handle de connection, disconnections and the data sent to the group

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,34 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
-    pass
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'tester_message',
+                'tester': 'esta es una prueba',
+            }
+        )
+
+    async def tester_message(self, event):
+        tester = event['tester']
+
+        await self.send(test_data=json.dumps({
+            'tester': tester
+        }))
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
