5680d9c34c382c82cf8b077235e4c521d9ecb45a
unknown <gregoriazuaje04@gmail.com>
2022-04-28 11:45:35 -0400

    setted configuration to deploy with asgi and daphne

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,6 +1,28 @@
 from .base import *
 import django_heroku
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
+        }
+    }
+}
+
+CHACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    }
+}
 
 django_heroku.settings(locals())
