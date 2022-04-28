1a4587b6b37f635922b69d7d28242549fa77b7e0
unknown <gregoriazuaje04@gmail.com>
2022-04-28 15:58:11 -0400

    setted for production

--- core/settings/production.py
+++ core/settings/production.py
@@ -6,6 +6,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+ALLOWED_HOSTS = ['localhost', 'channels-livechat.herokuapp.com']
+
+ASGI_APPLICATION = 'core.routing.application'
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
@@ -25,6 +29,6 @@ CHACHES = {
     }
 }
 
-django_heroku.settings(locals())
+CSRF_TRUSTED_ORIGINS = ['https://channels-livechat.herokuapp.com', 'localholst']
 
-CSRF_TRUSTED_ORIGINS = ["channels-livechat.herokuapp.com"]
+django_heroku.settings(locals())
